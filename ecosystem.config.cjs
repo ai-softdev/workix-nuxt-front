e41477cdca1b9b6e68e71ac6d4be6a9323cac24d
Narrowert <bringihorizdi4444@gmail.com>
2025-03-10 08:40:39 +0500

    esosystem update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ERP',
-      port: '3000',
+      port: '3001',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs'
